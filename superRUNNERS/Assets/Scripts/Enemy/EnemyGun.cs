db46631065f06b4ca2712728b79dea4080b38183
shui <78994143+blackmirag3@users.noreply.github.com>
2023-05-30 17:53:54 +0800

    Updated EnemyGun.cs
    Fixed enemy bullet velocity bug

--- superRUNNERS/Assets/Scripts/Enemy/EnemyGun.cs
+++ superRUNNERS/Assets/Scripts/Enemy/EnemyGun.cs
@@ -48,7 +48,8 @@ public class EnemyGun : MonoBehaviour
         float z = Random.Range(-inaccuracy, inaccuracy);
         Vector3 shotDir = playerPos.position - bulletPos.position + new Vector3(x, y, z);
         GameObject newBullet = Instantiate(currBullet, bulletPos.position, Quaternion.identity);
-        newBullet.transform.forward = shotDir.normalized;
+        shotDir = shotDir.normalized;
+        newBullet.transform.forward = shotDir;
         newBullet.GetComponent<Rigidbody>().AddForce(shotDir * bulletVelocity, ForceMode.Impulse);
         bulletsShot += 1f;
         if (bulletsShot < bulletsPerShot)
