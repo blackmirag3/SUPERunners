872111523534c25511bef3669150f1b914ff02b8
rismm <ryan.sim8344@gmail.com>
2023-06-29 19:32:25 +0800

    Added custom and persistent keybinds

--- superRUNNERS/Assets/Scripts/Menu/MainMenu.cs
+++ superRUNNERS/Assets/Scripts/Menu/MainMenu.cs
@@ -11,14 +11,57 @@ public class MainMenu : MonoBehaviour
     private GameObject settingsMenu;
     [SerializeField]
     private GameObject diffSelector;
+    [SerializeField]
+    private GameObject keybindMenu;
+
+    private bool rebinding;
 
     private KeyCode escapeKey = KeyCode.Escape;
+
+    private void Start()
+    {
+        rebinding = false;
+        menuState = CurrentMenu.main;
+    }
+
+    private enum CurrentMenu
+    {
+        main,
+        play,
+        settings,
+        keybinds,
+    }
+    private CurrentMenu menuState;
 
     private void Update()
     {
         if (Input.GetKeyDown(escapeKey))
         {
-            OpenMainMenu();
+            OnEscapePress();
+        }
+    }
+
+    private void OnEscapePress()
+    {
+        switch (menuState)
+        {
+            case CurrentMenu.main:
+                break;
+            case CurrentMenu.play:
+                menuState = CurrentMenu.main;
+                OpenMenu(menuState);
+                break;
+            case CurrentMenu.settings:
+                menuState = CurrentMenu.main;
+                OpenMenu(menuState);
+                break;
+            case CurrentMenu.keybinds:
+                if (!rebinding)
+                {
+                    menuState = CurrentMenu.settings;
+                    OpenMenu(menuState);
+                }
+                break;
         }
     }
 
@@ -34,20 +77,68 @@ public class MainMenu : MonoBehaviour
 
     public void OpenSettings()
     {
-        currentMenu.SetActive(false);
-        settingsMenu.SetActive(true);
+        menuState = CurrentMenu.settings;
+        OpenMenu(menuState);
     }
 
     public void OpenMainMenu()
     {
-        currentMenu.SetActive(true);
-        settingsMenu.SetActive(false);
-        diffSelector.SetActive(false);
+        menuState = CurrentMenu.main;
+        OpenMenu(menuState);
     }
 
     public void SelectDifficulty()
     {
-        currentMenu.SetActive(false);
-        diffSelector.SetActive(true);
+        menuState = CurrentMenu.play;
+        OpenMenu(menuState);
+    }
+
+    public void OpenKeyBinds()
+    {
+        menuState = CurrentMenu.keybinds;
+        OpenMenu(menuState);
+    }
+
+    public void RebindingActive()
+    {
+        Debug.Log("esc key disabled");
+        rebinding = true;
+    }
+
+    public void RebindingDone()
+    {
+        Debug.Log("esc key enabled");
+        StartCoroutine(ResetEsc());
+    }
+
+    private IEnumerator ResetEsc()
+    {
+        yield return null;
+        rebinding = false;
+    }
+
+    private void OpenMenu(CurrentMenu menu)
+    {
+        switch (menu)
+        {
+            case CurrentMenu.main:
+                currentMenu.SetActive(true);
+                diffSelector.SetActive(false);
+                settingsMenu.SetActive(false);
+                break;
+            case CurrentMenu.play:
+                currentMenu.SetActive(false);
+                diffSelector.SetActive(true);
+                break;
+            case CurrentMenu.settings:
+                currentMenu.SetActive(false);
+                keybindMenu.SetActive(false);
+                settingsMenu.SetActive(true);
+                break;
+            case CurrentMenu.keybinds:
+                settingsMenu.SetActive(false);
+                keybindMenu.SetActive(true);
+                break;
+        }
     }
 }
--- superRUNNERS/Assets/Scripts/Menu/PauseMenu.cs
+++ superRUNNERS/Assets/Scripts/Menu/PauseMenu.cs
@@ -2,39 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject currentMenu;
-    public GameObject settingsMenu;
+    [SerializeField]
+    private GameObject currentMenu;
+    [SerializeField]
+    private GameObject settingsMenu;
+    [SerializeField]
+    private GameObject keybindMenu;
     public bool gameIsPaused;
 
     private KeyCode escapeKey = KeyCode.Escape;
     private bool isOtherMenu;
 
+    private bool rebinding;
+
     public GameEvent onPause;
+
+    private PlayerInput inputs;
+
+    private void Awake()
+    {
+        inputs = InputManager.instance.PlayerInput;
+    }
+
+    private enum CurrentMenu
+    {
+        none,
+        pause,
+        settings,
+        keybinds,
+    }
+    private CurrentMenu menuState;
 
     private void Start()
     {
+        menuState = CurrentMenu.none;
         gameIsPaused = false;
+        rebinding = false;
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(escapeKey))
         {
-            if (isOtherMenu)
-                CloseSettings();
-            else if (!gameIsPaused)
-            {
+            OnEscapePress();
+        }
+    }
+
+    private void OnEscapePress()
+    {
+        switch (menuState)
+        {
+            case CurrentMenu.none:
+                menuState = CurrentMenu.pause;
                 PauseGame();
-                onPause.CallEvent(this, true);
-            }
-            else
-            {
+                break;
+            case CurrentMenu.pause:
+                menuState = CurrentMenu.none;
                 ResumeGame();
-                onPause.CallEvent(this, false);
-            }
+                break;
+            case CurrentMenu.settings:
+                menuState = CurrentMenu.pause;
+                OpenMenu(menuState);
+                break;
+            case CurrentMenu.keybinds:
+                if (!rebinding)
+                {
+                    menuState = CurrentMenu.settings;
+                    OpenMenu(menuState);
+                }
+                break;
         }
     }
 
@@ -51,34 +91,40 @@ public class PauseMenu : MonoBehaviour
 
     public void OpenSettings()
     {
-        isOtherMenu = true;
-        currentMenu.SetActive(false);
-        settingsMenu.SetActive(true);
+        menuState = CurrentMenu.settings;
+        OpenMenu(menuState);
     }
 
     public void CloseSettings()
     {
-        isOtherMenu = false;
-        currentMenu.SetActive(true);
-        settingsMenu.SetActive(false);
+        menuState = CurrentMenu.pause;
+        OpenMenu(menuState);
     }
 
     public void PauseGame()
     {
-        currentMenu.SetActive(true);
+        menuState = CurrentMenu.pause;
+        OpenMenu(menuState);
         Time.timeScale = 0f;
         gameIsPaused = true;
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
+
+        inputs.actions.Disable();
+        onPause.CallEvent(this, true);
     }
 
     public void ResumeGame()
     {
-        currentMenu.SetActive(false);
+        menuState = CurrentMenu.none;
+        OpenMenu(menuState);
         Time.timeScale = 1f;
         gameIsPaused = false;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        inputs.actions.Enable();
+        onPause.CallEvent(this, false);
     }
 
     public void BackToMainMenu()
@@ -92,5 +138,52 @@ public class PauseMenu : MonoBehaviour
         }
 
         SceneManager.LoadScene(0);
+    }
+
+    public void OpenKeyBinds()
+    {
+        menuState = CurrentMenu.keybinds;
+        OpenMenu(menuState);
+    }
+
+    public void RebindingActive()
+    {
+        Debug.Log("esc key disabled");
+        rebinding = true;
+    }
+
+    public void RebindingDone()
+    {
+        Debug.Log("esc key enabled");
+        StartCoroutine(ResetEsc());
+    }
+
+    private IEnumerator ResetEsc()
+    {
+        yield return null;
+        rebinding = false;
+    }
+
+    private void OpenMenu(CurrentMenu menu)
+    {
+        switch (menu)
+        {
+            case CurrentMenu.none:
+                currentMenu.SetActive(false);
+                break;
+            case CurrentMenu.pause:
+                currentMenu.SetActive(true);
+                settingsMenu.SetActive(false);
+                break;
+            case CurrentMenu.settings:
+                currentMenu.SetActive(false);
+                keybindMenu.SetActive(false);
+                settingsMenu.SetActive(true);
+                break;
+            case CurrentMenu.keybinds:
+                settingsMenu.SetActive(false);
+                keybindMenu.SetActive(true);
+                break;
+        }
     }
 }
