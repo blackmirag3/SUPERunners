3b13c10ac6663a848f0ea6700afea741788e3704
shui <78994143+blackmirag3@users.noreply.github.com>
2023-07-06 15:35:46 +0800

    Added retro skybox and lighting

---
+++ superRUNNERS/Assets/Scripts/GameLogic/Tutorial/TutorialControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialControl : MonoBehaviour
+{
+    //[SerializeField] private TutorialRoomControl currentRoom;
+    //[SerializeField] TutorialManager 
+    public GameEvent nextLevel;
+
+    internal void TutorialComplete()
+    {
+        //invoke next level event for room control
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ superRUNNERS/Assets/Scripts/GameLogic/Tutorial/TutorialSegmentControl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSegmentControl : MonoBehaviour
+{
+    public Collider col;
+    [SerializeField] private string playerTag;
+    [SerializeField] private GameObject nextSegment;
+    [SerializeField] private GameObject currentSegment;
+    //[SerializeField] private bool isLastRoom; //additional check on top of collider
+    public DoorControl door;
+
+    //public GameObject exitIndicator;
+
+    private void Start()
+    {
+        col = GetComponent<Collider>();
+        col.isTrigger = true;
+    }
+
+    public void EnableSegmentTrigger(Component sender, object data)
+    {
+        col.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            col.enabled = false;
+            if (door != null) door.Open();
+            if (nextSegment != null) nextSegment.SetActive(true);
+            currentSegment.SetActive(false);
+            //TODO next room indicator?
+            //exitIndicator.SetActive(false);
+        }
+    }
+/*
+    public void FinalRoomComplete(Component sender, object data)
+    {
+        //TODO
+    }
+    */
+}
