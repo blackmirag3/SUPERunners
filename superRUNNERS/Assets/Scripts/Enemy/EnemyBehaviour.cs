138f2d932363f0c53d9fe5067882697cf04c9784
rismm <ryan.sim8344@gmail.com>
2023-07-12 00:53:35 +0800

    Bug fix
    Gun pickup after thrown
    Enemy damaging player while staggered right after punching animation

--- superRUNNERS/Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ superRUNNERS/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -59,6 +59,8 @@ public class EnemyBehaviour : MonoBehaviour, IDamageable
     [SerializeField] private DifficultySettings diff;
     [SerializeField] private GameEvent onHit;
 
+    private Coroutine meleeing = null;
+
     private void Awake()
     {
         if (DifficultySelector.instance != null)
@@ -76,6 +78,15 @@ public class EnemyBehaviour : MonoBehaviour, IDamageable
         enemyHitSound.Play();
         enemy.isStopped = true;
         isStaggered = true;
+        if (meleeing != null)
+        {
+            Debug.Log("Stop melee");
+            rightHand.enabled = false;
+            leftHand.enabled = false;
+            StopCoroutine(meleeing);
+            meleeing = null;
+        }
+        
         enemyHealth -= damage;
         if (isArmed)
         {
@@ -216,7 +227,7 @@ public class EnemyBehaviour : MonoBehaviour, IDamageable
             anim.SetInteger("UnarmedIndex", Random.Range(0, 4));
             recentMelee = true;
 
-            StartCoroutine(PunchRoutine());
+            meleeing = StartCoroutine(PunchRoutine());
         }
     }
 
